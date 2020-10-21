19e5fb2ea842d68910c848280ecf5df210549a66
lightyears <lightyears1998@hotmail.com>
2020-10-21 23:22:21 +0800

    chore: 添加 eslint

---
+++ backend/src/app.ts
@@ -0,0 +1,17 @@
+import Koa from "koa";
+import { ApolloServer, gql } from "apollo-server-koa";
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = { Query: { hello: () => "Hello world!" } };
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+const app = new Koa();
+app.use(server.getMiddleware());
+
+app.listen({ port: 4000 }, () => console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`));
