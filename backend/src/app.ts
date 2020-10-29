fc133fd4dba77424e973de2954404c429b9183a0
lightyears1998 <lightyears1998@hotmail.com>
2020-10-29 23:50:33 +0800

    feat: Let's code

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,10 +2,23 @@ import Koa from "koa";
 import { ApolloServer } from "apollo-server-koa";
 import { UserResolver } from "./resolver";
 import { buildSchema } from "type-graphql";
-import { PORT } from "./config";
+import { printSchema } from "graphql";
+import { createConnection } from "typeorm";
+import { VAR_DIR, PORT } from "./config";
+import path from "path";
+import fs from "fs-extra";
 
 async function bootstrap() {
+  const dbPath = path.join(VAR_DIR, "./database.sqlite3");
+  await fs.ensureDir(path.dirname(dbPath));
+  console.log("💾 Using sqlite database: " + dbPath);
+
+  await createConnection({ type: "better-sqlite3", database: dbPath });
+
   const schema = await buildSchema({ resolvers: [UserResolver] });
+  const schemaPath = path.join(VAR_DIR, "./schema.graphql");
+  await fs.writeFile(schemaPath, printSchema(schema));
+  console.log("📁 Schema prints to: " + schemaPath);
 
   const server = new ApolloServer({ schema, playground: true });
 
--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,7 +1,11 @@
 import config from "config";
+import path from "path";
 
 const tryGetConfig = <T>(configPath: string, defaultValue: T) => {
   return config.has(configPath) ? config.get(configPath) : defaultValue;
 };
+
+
+export const VAR_DIR = path.join(process.cwd(), "./var");
 
 export const PORT = tryGetConfig("http.port", 4000);
