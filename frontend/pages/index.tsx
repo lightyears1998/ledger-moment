de72d4a4d612debbd626571deaca120e2b0d6d04
lightyears1998 <lightyears1998@hotmail.com>
2020-11-18 20:34:50 +0800

    todo: 补充若干功能

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -11,6 +11,18 @@ export default function HomePage(): JSX.Element {
       </Head>
 
       <h1>刻刻账本</h1>
+
+      <div>
+        <h2>自定义服务器</h2>
+        <button>转到“设置”</button>
+      </div>
+
+      <div>
+        <h2>账本管理</h2>
+        <button>新建账本</button>
+        <button>切换账本</button>
+        <button>移除账本</button>
+      </div>
     </div>
   );
 }
